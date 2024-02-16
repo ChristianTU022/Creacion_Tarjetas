331915f7e9a90cabea8e4d40c9b22519dfb7ce8f
Practicante De Produccion Pastas Doria <NCDRpracprod@ads.loc>
2024-02-16 14:25:04 -0500

    Primera Fase de Proyecto, Duplica la informacion de la BD principal

---
+++ Creacion_Tarjetas/main.js
@@ -0,0 +1,35 @@
+
+
+function duplicarDatos() {
+    //Conectar Sheets a AppScript
+    const sheet = SpreadsheetApp.openById('1pPWE_pS5tNcRabGIIymprfMo2TNWswZKNv5ovOZ95dY');
+    //Conectar Hojas especificas
+    const p_respuestas_form_1 = sheet.getSheetByName('Respuestas_Form_1');
+    const p_datos_creacion_tarjetas = sheet.getSheetByName('Datos_Creacion_Tarjetas');
+    
+    const ultimaFila = p_respuestas_form_1.getLastRow();
+    
+    for (let fila = 2; fila <= ultimaFila; fila++) 
+    {
+      Logger.log('Fila= ' + fila);
+      const fecha = p_respuestas_form_1.getRange('A' + fila).getValue();
+      const descripcion = p_respuestas_form_1.getRange('F' + fila).getValue();
+      const descripcionDetallada = p_respuestas_form_1.getRange('G' + fila).getValue();
+      const tituloTarjeta = p_respuestas_form_1.getRange('I' + fila).getValue() + '' +
+                            p_respuestas_form_1.getRange('J' + fila).getValue() + '' +
+                            p_respuestas_form_1.getRange('K' + fila).getValue() + '' +
+                            p_respuestas_form_1.getRange('L' + fila).getValue() + '' +
+                            p_respuestas_form_1.getRange('N' + fila).getValue();
+      const nombrePersona = p_respuestas_form_1.getRange('D' + fila).getValue();
+      const lugar = p_respuestas_form_1.getRange('C' + fila).getValue();
+      const grupoPlanificador = p_respuestas_form_1.getRange('O' + fila).getValue();
+      const prioridad = p_respuestas_form_1.getRange('T' + fila).getValue();
+      const tipoRiesgo = p_respuestas_form_1.getRange('H' + fila).getValue();
+    
+      if (fecha !== '') 
+      {
+        Logger.log("Entre a imprimir")
+        p_datos_creacion_tarjetas.appendRow([fecha, descripcion, descripcionDetallada, tituloTarjeta, nombrePersona, lugar, grupoPlanificador, prioridad, tipoRiesgo]);
+      }
+    }
+  }
