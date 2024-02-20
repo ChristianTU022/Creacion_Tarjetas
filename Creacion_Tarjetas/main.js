b0eea5709bced2297b7f4677a88d2c304b9da12b
christiantu022 <cratrianac@udistrital.edu.co>
2024-02-20 13:52:45 -0500

    Creacion de Condicional que agrega cod_titulo a la fila K

--- Creacion_Tarjetas/main.js
+++ Creacion_Tarjetas/main.js
@@ -22,7 +22,7 @@ function limpiarDatosSalida() {
   const p_Datos_Salida_CT = sheet.getSheetByName('Datos_Salida_CT');
 
   const ultimaFila = p_Datos_Salida_CT.getLastRow();
-  p_Datos_Salida_CT.getRange('A2:J' + ultimaFila).clearContent();
+  p_Datos_Salida_CT.getRange('A2:K' + ultimaFila).clearContent();
 }
 
 
@@ -38,7 +38,6 @@ function duplicarDatos() {
     //Saca Los datos especificos del Archivo/hoja de Entrada al archivo/hoja de salida
     for (let fila = 2; fila <= ultimaFila; fila++) 
     {
-      Logger.log('Fila= ' + fila);
       const fecha = p_Datos_Entrada_CT.getRange('A' + fila).getValue();
       const descripcion = p_Datos_Entrada_CT.getRange('F' + fila).getValue();
       const descripcionDetallada = p_Datos_Entrada_CT.getRange('G' + fila).getValue();
@@ -56,8 +55,30 @@ function duplicarDatos() {
 
       if (fecha !== '') 
       {
-        Logger.log("Entre a imprimir")
         p_Datos_Salida_CT.appendRow([fecha, descripcion, descripcionDetallada, tituloTarjeta, nombrePersona, lugar, grupoPlanificador, prioridad, tipoRiesgo]);
       }
+      let valor_Cod_Titulo = p_Datos_Salida_CT.getRange('D' + fila).getValue();
+      Logger.log(valor_Cod_Titulo + " Valor Tomado");
+      if (valor_Cod_Titulo === 'Condición básica' || valor_Cod_Titulo === 'Condiciones básicas') 
+      {
+        p_Datos_Salida_CT.getRange('K' + fila).setValue('CB');
+      } else if (valor_Cod_Titulo === 'Condición insegura') {
+        p_Datos_Salida_CT.getRange('K' + fila).setValue('CI');
+      } else if (valor_Cod_Titulo === 'Incidente') {
+        p_Datos_Salida_CT.getRange('K' + fila).setValue('I');
+      } else if (valor_Cod_Titulo === 'Acto inseguro' || valor_Cod_Titulo === 'Actos Inseguros') {
+        p_Datos_Salida_CT.getRange('K' + fila).setValue('AI');
+      } else if (valor_Cod_Titulo === 'Incidentes ambientales') {
+        p_Datos_Salida_CT.getRange('K' + fila).setValue('IA');
+      } else if (valor_Cod_Titulo === 'Acto Inseguro ambientales') {
+        p_Datos_Salida_CT.getRange('K' + fila).setValue('AIA');
+      } else if (valor_Cod_Titulo === 'Defecto') {
+        p_Datos_Salida_CT.getRange('K' + fila).setValue('DF');
+      } else if (valor_Cod_Titulo === 'Acto y/o comportamiento') {
+        p_Datos_Salida_CT.getRange('K' + fila).setValue('AC');
+      } else if (valor_Cod_Titulo === 'Condición de operación') {
+        p_Datos_Salida_CT.getRange('K' + fila).setValue('CO');
+      }    
     }
   }
+
