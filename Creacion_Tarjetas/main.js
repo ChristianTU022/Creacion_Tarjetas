8bd37fbda94e2e19552c192a6922fc7903633c68
christiantu022 <cratrianac@udistrital.edu.co>
2024-02-20 11:45:05 -0500

    Creacion de Menu Personalizado, poder limpiar los datos de E y S

--- Creacion_Tarjetas/main.js
+++ Creacion_Tarjetas/main.js
@@ -1,46 +1,63 @@
-var ui = SpreadsheetApp.getUi();
-function onOpen(){
-  ui.createMenu('Menu Personalizado').addItem('Duplicar Los Datos', 'duplicarDatos').addToUi();
+
+//Funcion para Generar Alertas, Menus Personalizados, etc.
+function onOpen() {
+  const ui = SpreadsheetApp.getUi();
+  ui.createMenu('Menú Personalizado')
+    .addItem('Duplicar Datos', 'duplicarDatos')
+    .addItem('Limpiar Datos de Entrada', 'limpiarDatosEntrada')
+    .addItem('Limpiar Datos de Salida', 'limpiarDatosSalida')
+    .addToUi();
 }
 
-function vaciarDatosSalida() {
+//Funcion que permite Limpiar los datos del formulario sheets la hoja "Datos_Entrada_CT"
+function limpiarDatosEntrada() {
   const sheet = SpreadsheetApp.openById('1pPWE_pS5tNcRabGIIymprfMo2TNWswZKNv5ovOZ95dY');
-  const p_datos_creacion_tarjetas = sheet.getSheetByName('Datos_Creacion_Tarjetas');
-
-  const ultimaFila = p_datos_creacion_tarjetas.getLastRow();
-  p_datos_creacion_tarjetas.getRange('A2:J' + ultimaFila).clearContent();
+  const p_Datos_Salida_CT = sheet.getSheetByName('Datos_Entrada_CT');
+  const ultimaFila = p_Datos_Salida_CT.getLastRow();
+  p_Datos_Salida_CT.getRange('A2:AK' + ultimaFila).clearContent();
 }
+//Funcion que permite Limpiar los datos del formulario sheets la hoja "Datos_Salida_CT"
+function limpiarDatosSalida() {
+  const sheet = SpreadsheetApp.openById('1pPWE_pS5tNcRabGIIymprfMo2TNWswZKNv5ovOZ95dY');
+  const p_Datos_Salida_CT = sheet.getSheetByName('Datos_Salida_CT');
+
+  const ultimaFila = p_Datos_Salida_CT.getLastRow();
+  p_Datos_Salida_CT.getRange('A2:J' + ultimaFila).clearContent();
+}
+
 
 function duplicarDatos() {
     //Conectar Sheets a AppScript
     const sheet = SpreadsheetApp.openById('1pPWE_pS5tNcRabGIIymprfMo2TNWswZKNv5ovOZ95dY');
     //Conectar Hojas especificas
-    const p_respuestas_form_1 = sheet.getSheetByName('Respuestas_Form_1');
-    const p_datos_creacion_tarjetas = sheet.getSheetByName('Datos_Creacion_Tarjetas');
+    const p_Datos_Entrada_CT = sheet.getSheetByName('Datos_Entrada_CT');
+    const p_Datos_Salida_CT = sheet.getSheetByName('Datos_Salida_CT');
     
-    const ultimaFila = p_respuestas_form_1.getLastRow();
+    const ultimaFila = p_Datos_Entrada_CT.getLastRow();
     
+    //Saca Los datos especificos del Archivo/hoja de Entrada al archivo/hoja de salida
     for (let fila = 2; fila <= ultimaFila; fila++) 
     {
       Logger.log('Fila= ' + fila);
-      const fecha = p_respuestas_form_1.getRange('A' + fila).getValue();
-      const descripcion = p_respuestas_form_1.getRange('F' + fila).getValue();
-      const descripcionDetallada = p_respuestas_form_1.getRange('G' + fila).getValue();
-      const tituloTarjeta = p_respuestas_form_1.getRange('I' + fila).getValue() + '' +
-                            p_respuestas_form_1.getRange('J' + fila).getValue() + '' +
-                            p_respuestas_form_1.getRange('K' + fila).getValue() + '' +
-                            p_respuestas_form_1.getRange('L' + fila).getValue() + '' +
-                            p_respuestas_form_1.getRange('N' + fila).getValue();
-      const nombrePersona = p_respuestas_form_1.getRange('D' + fila).getValue();
-      const lugar = p_respuestas_form_1.getRange('C' + fila).getValue();
-      const grupoPlanificador = p_respuestas_form_1.getRange('O' + fila).getValue();
-      const prioridad = p_respuestas_form_1.getRange('T' + fila).getValue();
-      const tipoRiesgo = p_respuestas_form_1.getRange('H' + fila).getValue();
-    
+      const fecha = p_Datos_Entrada_CT.getRange('A' + fila).getValue();
+      const descripcion = p_Datos_Entrada_CT.getRange('F' + fila).getValue();
+      const descripcionDetallada = p_Datos_Entrada_CT.getRange('G' + fila).getValue();
+      const tituloTarjeta = p_Datos_Entrada_CT.getRange('I' + fila).getValue() + '' +
+                            p_Datos_Entrada_CT.getRange('J' + fila).getValue() + '' +
+                            p_Datos_Entrada_CT.getRange('K' + fila).getValue() + '' +
+                            p_Datos_Entrada_CT.getRange('L' + fila).getValue() + '' +
+                            p_Datos_Entrada_CT.getRange('N' + fila).getValue();
+      const nombrePersona = p_Datos_Entrada_CT.getRange('D' + fila).getValue();
+      const lugar = p_Datos_Entrada_CT.getRange('C' + fila).getValue();
+      const grupoPlanificador = p_Datos_Entrada_CT.getRange('O' + fila).getValue();
+      const prioridad = p_Datos_Entrada_CT.getRange('T' + fila).getValue();
+      const tipoRiesgo = p_Datos_Entrada_CT.getRange('H' + fila).getValue();
+       
+
       if (fecha !== '') 
       {
         Logger.log("Entre a imprimir")
-        p_datos_creacion_tarjetas.appendRow([fecha, descripcion, descripcionDetallada, tituloTarjeta, nombrePersona, lugar, grupoPlanificador, prioridad, tipoRiesgo]);
+        p_Datos_Salida_CT.appendRow([fecha, descripcion, descripcionDetallada, tituloTarjeta, nombrePersona, lugar, grupoPlanificador, prioridad, tipoRiesgo]);
       }
     }
   }
